472d1f89b0f8125c6691119f155d67598c6699f3
ceceyjwww <71691107+ceceyjwww@users.noreply.github.com>
2025-07-18 20:20:30 -0400

    Add solution for reorder-list

---
+++ leetcode/reorder-list.py
@@ -0,0 +1,40 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fast = head
+        slow = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+
+
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        return prev
+
+
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        mid = self.middleNode(head)
+        head2 = self.reverseList(mid)
+        while head2.next:
+            nxt = head.next
+            nxt2 = head2.next
+            head2.next = nxt
+            head.next = head2
+            head2 = nxt2
+            head = nxt
